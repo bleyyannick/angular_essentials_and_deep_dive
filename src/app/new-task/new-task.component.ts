3c3049ddb9c6ff9a69f2f1f02d74e125404e736d
Yannick Bley <bleyyannickpro@gmail.com>
2024-07-12 11:32:10 +0200

    feat: retrive form input user data with angular reactive form

--- src/app/new-task/new-task.component.ts
+++ src/app/new-task/new-task.component.ts
@@ -1,27 +1,28 @@
 import { Component, output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   template:`
     <div class="backdrop" (click)="closeNewTask()"></div>
     <dialog open>
       <h2>Add Task</h2>
-      <form>
+      <form [formGroup]="newTaskForm" (submit)="submitFormTask()">
         <p>
           <label for="title">Title</label>
-          <input type="text" id="title" name="title" />
+          <input type="text" id="title" name="title" formControlName="taskTitle" />
         </p>
 
         <p>
           <label for="summary">Summary</label>
-          <textarea id="summary" rows="5" name="summary"></textarea>
+          <textarea id="summary" rows="5" name="summary" formControlName="taskSummary"></textarea>
         </p>
 
         <p>
           <label for="due-date">Due Date</label>
-          <input type="date" id="due-date" name="due-date" />
+          <input type="date" id="due-date" name="due-date" formControlName="taskDate" />
         </p>
 
         <p class="actions">
@@ -34,9 +35,21 @@ import { Component, output } from '@angular/core';
 })
 export class NewTaskComponent {
   toDisableNewTask = output<boolean>();
+  newTaskForm = new FormGroup({
+    taskTitle: new FormControl(''),
+    taskSummary: new FormControl(''),
+    taskDate: new FormControl('')
+  });
 
   closeNewTask(): void {
     this.toDisableNewTask.emit(false);
   }
 
+  submitFormTask(): void {
+    console.log(this.newTaskForm.value.taskTitle);
+    console.log(this.newTaskForm.value.taskSummary);
+    console.log(this.newTaskForm.value.taskDate);
+  } 
+  
+  
 }
